4a11e120d4bbb9d6e8e75e672879bad6e157c72b
Dylan Huang <dylan.p.huang@gmail.com>
2016-08-02 03:50:41 -0700

    Fixed log out bug: Can't verify CSRF token authenticity...Completed 422 Unprocessable Entity in 1ms (ActiveRecord: 0.0ms)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   rescue_from Poke::API::Errors::LoginFailure, :with => :login_error
+  rescue_from ActionController::InvalidAuthenticityToken, :with => logout_error
 
   def new
   end
@@ -49,5 +50,8 @@ class SessionsController < ApplicationController
     def login_error
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
+    end
+    def logout_error
+      redirect_to 'new'
     end
 end
