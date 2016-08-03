2d9b34f5259165a8405f35efe9444449c19d65c1
Dylan Huang <dylan.p.huang@gmail.com>
2016-08-03 00:11:40 -0700

    Cleaned up Pokemon card Partial and fixed Missingno pokemon

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -35,48 +35,48 @@ module SessionsHelper
     pokemon_hash = JSON.parse(file)
 
     #begin
-      response[:GET_INVENTORY][:inventory_delta][:inventory_items].each do |item|
-        item[:inventory_item_data].each do |type, i|
-          case type
-          when :item 
-            if i != nil 
-              item_id = i[:item_id]
-              count = i[:count]
-              user.items.create(item_id: item_id, count: count)
-            end
-          when :pokemon_data
-            if i != nil
-              # Set poke_id
-              poke_id = i[:pokemon_id].capitalize.to_s
-              # To deal with Nidoran naming
-              poke_id.match('Nidoran_female') ? poke_id = 'Nidoran♀' : nil
-              poke_id.match('Nidoran_male') ? poke_id = 'Nidoran♂' : nil
+    response[:GET_INVENTORY][:inventory_delta][:inventory_items].each do |item|
+      item[:inventory_item_data].each do |type, i|
+        case type
+        when :item 
+          if i != nil 
+            item_id = i[:item_id]
+            count = i[:count]
+            user.items.create(item_id: item_id, count: count)
+          end
+        when :pokemon_data
+          if i != nil
+            # Set poke_id
+            poke_id = i[:pokemon_id].capitalize.to_s
+            # To deal with Nidoran naming
+            poke_id.match('Nidoran_female') ? poke_id = 'Nidoran♀' : nil
+            poke_id.match('Nidoran_male') ? poke_id = 'Nidoran♂' : nil
 
-              # Set poke_num
-              poke_num = ""
-              # To deal with MISSINGNO Pokemon
-              if pokemon_hash.key(poke_id) != nil
-                poke_num = format("%03d", pokemon_hash.key(poke_id))
-              else
-                poke_num = "000"
-              end
-
-              # Save data
-              move_1 = i[:move_1]
-              move_2 = i[:move_2]
-              health = i[:stamina_max]
-              attack = i[:individual_attack]
-              defense = i[:individual_defense]
-              stamina = i[:individual_stamina]
-              cp = i[:cp]
-              iv = ((attack + defense + stamina) / 45.0).round(2)
-              user.pokemon.create(:poke_id => poke_id, :poke_num => poke_num, :move_1 => move_1, :move_2 => move_2, :health => health, :attack => attack, :defense => defense, :stamina => stamina, :iv => iv, :cp => cp)
+            # Set poke_num
+            poke_num = ""
+            # To deal with MISSINGNO Pokemon
+            if pokemon_hash.key(poke_id) != nil
+              poke_num = format("%03d", pokemon_hash.key(poke_id))
+            else
+              poke_num = "000"
             end
+
+            # Save data
+            move_1 = i[:move_1]
+            move_2 = i[:move_2]
+            health = i[:stamina_max]
+            attack = i[:individual_attack]
+            defense = i[:individual_defense]
+            stamina = i[:individual_stamina]
+            cp = i[:cp]
+            iv = ((attack + defense + stamina) / 45.0).round(2)
+            user.pokemon.create(:poke_id => poke_id, :poke_num => poke_num, :move_1 => move_1, :move_2 => move_2, :health => health, :attack => attack, :defense => defense, :stamina => stamina, :iv => iv, :cp => cp)
           end
         end
       end
+    end
     # Cleanup error pokemonn
-    Pokemon.delete_all("poke_id = 'MISSINGNO'")
+    Pokemon.where(poke_id: "Missingno").delete_all
     return true
   end
 
@@ -84,36 +84,36 @@ module SessionsHelper
   def get_name(client)
     call = get_call(client, :get_player)
     begin
-    name = call.response[:GET_PLAYER][:player_data][:username].downcase
+      name = call.response[:GET_PLAYER][:player_data][:username].downcase
     rescue NoMethodError
       logger.debug "Rescued from get_name"
       get_name(client)
     end
   end
-   
+
   # Handle login logic
   def setup_client(client)
     #if !params[:user].nil? # PTC LOGIN------------
-      # Grab all credentials from form
-      username = params[:user][:username]
-      pass = params[:user][:password]
-      auth = params[:user][:auth]
-      client.login(username, pass, auth)
-      # Create new user if user is new, otherwise retrieve it
-      # Keep requesting until response
-      name = get_name(client)
-      @user = User.where(:name => name).first_or_create!
+    # Grab all credentials from form
+    username = params[:user][:username]
+    pass = params[:user][:password]
+    auth = params[:user][:auth]
+    client.login(username, pass, auth)
+    # Create new user if user is new, otherwise retrieve it
+    # Keep requesting until response
+    name = get_name(client)
+    @user = User.where(:name => name).first_or_create!
     #end
     #else             # GOOGLE LOGIN---------
-      #@user = User.from_omniauth(env["omniauth.auth"])
-      #google = Poke::API::Auth::GOOGLE.new(@user.name, "password")
-      #google.instance_variable_set(:@access_token, @user.access_token)
-      #client.instance_variable_set(:@auth, google)
-      #client.instance_eval{ fetch_endpoint }
-      #debugger
-      #name = get_name(client)
-      #@user.update_attribute(:name, name)
-      #return @user
+    #@user = User.from_omniauth(env["omniauth.auth"])
+    #google = Poke::API::Auth::GOOGLE.new(@user.name, "password")
+    #google.instance_variable_set(:@access_token, @user.access_token)
+    #client.instance_variable_set(:@auth, google)
+    #client.instance_eval{ fetch_endpoint }
+    #debugger
+    #name = get_name(client)
+    #@user.update_attribute(:name, name)
+    #return @user
     #end
   end 
 
