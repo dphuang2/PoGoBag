41cd69f6bf9253028aa8a1948ead29cba271e418
Dylan Huang <dylan.p.huang@gmail.com>
2016-08-04 22:27:16 -0700

    Fixed image path for some images

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -2,11 +2,11 @@ module StatsHelper
   def rank_image(rank)
     case rank
     when 1
-      return "/assets/etc/badge_lv3.png"
+      return image_path "/assets/etc/badge_lv3.png"
     when 2
-      return "/assets/etc/badge_lv2.png"
+      return image_path "/assets/etc/badge_lv2.png"
     when 3
-      return "/assets/etc/badge_lv1.png"
+      return image_path "/assets/etc/badge_lv1.png"
     end
   end
 end
