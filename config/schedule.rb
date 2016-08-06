da0f3f4e56a8dd19bc8e329b01b52f5b5b3dbe9a
Dylan Huang <dylan.p.huang@gmail.com>
2016-08-05 17:29:25 -0700

    Fix script/runner error

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,7 @@
 set :output, "#{path}/log/cron.log"
+
+job_type :custom_runner,  
+  "cd :path && bundle exec rails runner -e :environment ':task' :output"
 every 1.minute do
-  runner "User.scheduled_refresh"
+  custom_runner "User.scheduled_refresh"
 end
