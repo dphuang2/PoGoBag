43228edc5032501a3fdd5e311fe00a0bfab774a2
Dylan Huang <dylan.p.huang@gmail.com>
2016-08-05 23:29:43 -0700

    Fixed formatting of string on Top 100

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -9,4 +9,15 @@ module StatsHelper
       return image_path "etc/badge_lv1.png"
     end
   end
+
+  def format_move(move)
+    move.slice! '_FAST'
+    move = move.split '_'
+    move = move.map do |word|
+      word.capitalize
+    end
+    move = move.join(" ")
+    return move
+  end
+    
 end
