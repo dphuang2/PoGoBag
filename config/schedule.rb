914338ed61f893f47fa2e765e5d4041f3e163dfc
Dylan Huang <dylan.p.huang@gmail.com>
2016-08-05 17:59:34 -0700

    Try re-adding bin to git

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,5 @@
 set :output, "#{path}/log/cron.log"
 
-job_type :custom_runner,  
-  "cd :path && bundle exec rails runner -e :environment ':task' :output"
 every 1.minute do
-  custom_runner "User.scheduled_refresh"
+  runner "User.scheduled_refresh"
 end
