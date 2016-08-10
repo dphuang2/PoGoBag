7d631ad68ee475b058b0cca61924bee6bf3822e6
Dylan Huang <dylan.p.huang@gmail.com>
2016-08-10 12:12:17 -0700

    Limit Top 100 to 100...

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,9 +1,9 @@
 class StatsController < ApplicationController
   def show
     if params[:stat]
-      @pokemon = Pokemon.order("#{params[:stat]} DESC, cp DESC, iv DESC")
+      @pokemon = Pokemon.order("#{params[:stat]} DESC, cp DESC, iv DESC").limit(100).includes(:user)
     else
-      @pokemon = Pokemon.order("cp DESC, iv DESC")
+      @pokemon = Pokemon.order("cp DESC, iv DESC").limit(100).includes(:user)
     end
   end
 end
