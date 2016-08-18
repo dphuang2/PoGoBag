b303ca648e5b8428bdac68d1f1aa2cc93afa0771
Dylan Huang <dylan.p.huang@gmail.com>
2016-08-17 23:19:31 -0500

    Added prepend: true to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception, prepend: true
   require 'poke-api'
   include SessionsHelper
   include UsersHelper
